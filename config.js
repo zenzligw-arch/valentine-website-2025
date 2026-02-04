e013fd89f4213e8e5a0b9674e5f82f08d34d3f4f
zenzligw-arch <zenzligw@gmail.com>
2026-02-04 02:16:21 -0300

    Update config.js

--- config.js
+++ config.js
@@ -77,7 +77,7 @@ const CONFIG = {
     // Add your own music URL after getting proper licenses
     music: {
         enabled: true,                     // Music feature is enabled
-        autoplay: true,                    // Try to autoplay (note: some browsers may block this)
+        autoplay: false,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://res.cloudinary.com/dy15rvwfi/video/upload/v1770181618/George_Michael_-_Careless_Whisper_Official_Video_d0ecf8.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
