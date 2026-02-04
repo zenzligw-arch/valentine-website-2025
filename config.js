e772aac8f4a7686428445a51586ee789423e02c2
zenzligw-arch <zenzligw@gmail.com>
2026-02-04 02:10:49 -0300

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
         musicUrl: "https://res.cloudinary.com/dy15rvwfi/video/upload/v1770181618/George_Michael_-_Careless_Whisper_Official_Video_d0ecf8.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 1.0                        // Volume level (0.0 to 1.0)
     }
 };
 
